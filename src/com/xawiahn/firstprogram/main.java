f331a38b9b5360db58cc54b23c28c299762f5cb7
Xawiahn <xawiahn@gmail.com>
2012-08-23 12:54:03 +0200

    Added another printline

--- src/com/xawiahn/firstprogram/main.java
+++ src/com/xawiahn/firstprogram/main.java
@@ -7,7 +7,7 @@ public class main {
 	 */
 	public static void main(String[] args) {
 		System.out.println("This is my first program!");
-
+		System.out.println("This is my first program!");
 	}
 
 }
