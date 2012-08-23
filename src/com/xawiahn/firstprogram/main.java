bfce267b5776b797d43204b4d1c84e737717244a
Xawiahn <xawiahn@gmail.com>
2012-08-23 12:50:23 +0200

    Created main.java and added some printline for my first project!

---
+++ src/com/xawiahn/firstprogram/main.java
@@ -0,0 +1,13 @@
+package com.xawiahn.firstprogram;
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is my first program!");
+
+	}
+
+}
