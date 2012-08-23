b2556c7ba2ba4b0eeb12021dcb6696755575e725
StaggVilde <cj.kalmar@gmail.com>
2012-08-23 14:18:41 +0300

    Relevant
    Läs sidan 3

--- src/com/xawiahn/firstprogram/main.java
+++ src/com/xawiahn/firstprogram/main.java
@@ -6,7 +6,7 @@ public class main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("This is my first program!");
+		System.out.println("This is my first program!"); //Hur tänkte du här?
 		System.out.println("This is another line =)");
 	}
 
